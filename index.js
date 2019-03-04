a0bd332d6ff9ed986c9903415fb7af918c7c0445
Meet Shah <meetdshah007@gmail.com>
2019-03-04 13:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,16 +60,12 @@ function createRock(x) {
 }
 
 function endGame() {
-  while(ROCKS.length > 0){
-    GAME.removeChild(ROCKS[0]);
-    ROCKS.shift();
-  }
-
   clearInterval(gameInterval);
-  window.removeEventListener('keydown', moveDodger);
-  alert("YOU LOSE!");
-  START.innerHTML = 'Play again?';
-  START.style.display = 'inline';
+  ROCKS.forEach(function(rock)
+      {rock.remove()
+      });
+  document.removeEventListener('keydown',moveDodger);
+  return alert('YOU LOSE!');
 }
 
 function moveDodger(e) {
